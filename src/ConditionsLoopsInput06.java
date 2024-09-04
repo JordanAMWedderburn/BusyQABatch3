9c4ca349db1e362dcd51de8f6eff5765f82e1f2a
JordanAMWedderburn <J.am.wedderburn@gmail.com>
2024-09-04 19:38:22 -0400

    Update ConditionsLoopsInput06.java

--- src/ConditionsLoopsInput06.java
+++ src/ConditionsLoopsInput06.java
@@ -2,19 +2,18 @@ import java.util.Scanner;
 
 class ConditionsLoopsInput06 {
   public static void main(String[] args) {
-    Scanner myObj = new Scanner(System.in);
-
-    int count = 0;
-    System.out.println("Enter a number: ");
-    int num = myObj.nextInt();
+   Scanner myObj = new Scanner(System.in);
+   int count = 0;
+   System.out.println("Enter a number: ");
+   int num = myObj.nextInt();
 
     while (num != 0) {
       // num = num/10
       num /= 10;
       ++count;
     }
-
     System.out.println("Number of digits: " + count);
-   }
+    myObj.close();
+  }
   }
 
