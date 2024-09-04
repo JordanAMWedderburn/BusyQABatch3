440cce46699f2e6225b20b556ce8b583ec553167
JordanAMWedderburn <J.am.wedderburn@gmail.com>
2024-09-04 19:33:24 -0400

    Update ConditionsLoopsInput02.java

--- src/ConditionsLoopsInput02.java
+++ src/ConditionsLoopsInput02.java
@@ -2,11 +2,12 @@ import java.util.Scanner;
 
 class ConditionsLoopsInput02 {
   public static void main(String[] args) {
-    Scanner myObj = new Scanner(System.in);
-    System.out.println("Enter 3 numbers:");
-    int myNum1 = myObj.nextInt();
-    int myNum2 = myObj.nextInt();
-    int myNum3 = myObj.nextInt();
+	  Scanner myObj = new Scanner(System.in);
+	  System.out.println("Enter 3 numbers:");
+	  int myNum1 = myObj.nextInt();
+	  int myNum2 = myObj.nextInt();
+	  int myNum3 = myObj.nextInt();
+  
       if (myNum1 > myNum2 & myNum1 > myNum3) {
       System.out.println("The greatest number is: " + myNum1);
     } else if (myNum2 > myNum1 & myNum2 > myNum3) {
@@ -14,5 +15,6 @@ class ConditionsLoopsInput02 {
     } else {
         System.out.println("The greatest number is: " + myNum3);
     }
+      myObj.close();
   }
 }
