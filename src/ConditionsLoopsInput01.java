59580b5d0723ea95745aa5bd21959bd80e645d29
JordanAMWedderburn <J.am.wedderburn@gmail.com>
2024-09-04 19:31:51 -0400

    Update ConditionsLoopsInput01.java

--- src/ConditionsLoopsInput01.java
+++ src/ConditionsLoopsInput01.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 
 class ConditionsLoopsInput01{
   public static void main(String[] args) {
-    Scanner myObj = new Scanner(System.in);
-    System.out.println("Enter a number:");
-    int myNum = myObj.nextInt();
+  Scanner myObj = new Scanner(System.in);
+  System.out.println("Enter a number:");
+  
+  int myNum = myObj.nextInt();
+  
       if (myNum > 0) {
       System.out.println("The value is a positive number.");
     } else if (myNum < 0) {
@@ -12,5 +14,6 @@ class ConditionsLoopsInput01{
     } else {
       System.out.println("The value is 0.");
     }
+  myObj.close();
   }
 }
